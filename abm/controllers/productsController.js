122a2f7407e4db1e0b7a79d52e649b34df173970
juliandiaz27 <julianmatiasdiaz6@gmail.com>
2020-11-22 22:24:45 -0300

    terminado

--- abm/controllers/productsController.js
+++ abm/controllers/productsController.js
@@ -1,7 +1,78 @@
+const fs = require("fs")
+const data = fs.readFileSync("./data.json",{ encoding: "utf-8"})
+const dataParse = JSON.parse(data)
 const productsController = {
-    create : (req , res , next) =>{
+    create : (req , res )  =>{
         res.render ("formCreate")
+
+    },
+    createProduct : (req ,res, next)=>{
+        /* ------------------------------NUEVO OBJETO---------------------- */
+        
+        let newProduct = {
+            id:  dataParse.length +1,
+            nombre: req.body.nombre ,
+            precio: req.body.precio,
+            marca: req.body.marca,
+            descripcion: req.body.descripcion ,
+            stock: req.body.stock           
+        }
+        dataParse.push(newProduct)
+        let productSave = JSON.stringify(dataParse)
+        fs.writeFileSync("./data.json",productSave)
+        res.send("Producto Creado")
+    },
+
+    edit: (req,res,next)=>{
+       for (let i = 0; i < dataParse.length; i++) {
+           if (dataParse[i].id == req.params.id) {
+                return res.render("edit", {product: dataParse[req.params.id]})
+
+           }
+                      
+       }return res.send("No existe este producto")
+       
+        
+    },
+    actionEdit : (req,res,next)=>{
+        /* --------------------------EDITAR PRODUCTO-------------------------------- */
+        let editProduct = {
+            id:  req.params.id,
+            nombre: req.body.nombre ,
+            precio: req.body.precio,
+            marca: req.body.marca,
+            descripcion: req.body.descripcion ,
+            stock: req.body.stock           
+        }
+         for (let i = 0; i < dataParse.length; i++) {
+             if (editProduct.id == dataParse[i].id) {
+                 
+                 dataParse[i].nombre = editProduct.nombre
+                 dataParse[i].precio = editProduct.precio
+                 dataParse[i].marca = editProduct.marca
+                 dataParse[i].descripcion = editProduct.descripcion
+                 dataParse[i].stock = editProduct.stock
+
+                let productSave = JSON.stringify(dataParse)
+                fs.writeFileSync("./data.json",productSave)  
+                res.render("edit",{product:dataParse[i]} )               
+             }
+                        
+         } 
+    },
+    delete: (req,res,next)=>{
+        /* -------------------------BORRAR PRODUCTOS------------------------------- */
+        let nuevoArray = dataParse.filter(function(product){
+            return product.id != req.params.id 
+            
+
+        })
+                let productSave = JSON.stringify(nuevoArray)
+                fs.writeFileSync("./data.json",productSave)  
+                res.send("Producto eliminado") 
     }
 
 }
-module.export = productsController 
+
+
+module.exports = productsController 
--- abm/routes/products.js
+++ abm/routes/products.js
@@ -8,14 +8,15 @@ router.get("/" , function(req,res,next){
 
 })
 
-/* router.get("/create", function (req, res ,next){
-    res.send ("hola")
-}) */
+router.get("/create", productsController.create)
+ 
+router.post("/create", productsController.createProduct)
 
+router.get("/edit/:id", productsController.edit )
 
-router.get('/create', (req,res)=>{
-    res.send("hola")
+router.post("/edit/:id", productsController.actionEdit)
 
-})
+router.post("/delete/:id", productsController.delete)
+
 
 module.exports = router;
